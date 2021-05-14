7044671c94e00c0f951d0107d55fff3fd99713e3
Ilker Kesen <ilkerksn@gmail.com>
2021-05-14 22:49:28 +0300

    make it appropriate for our dataset

--- intphys/extra/tvqa/tvqa_abc.py
+++ intphys/extra/tvqa/tvqa_abc.py
@@ -17,82 +17,42 @@ class ABC(nn.Module):
         vid_feat_size = config["vid_feat_size"] # 2048
         embedding_size = config["embed_size"] # 300
         vocab_size = config["input_size"]
+        num_classes = config["output_size"]
 
         self.embedding = nn.Embedding(vocab_size, embedding_size)
         self.bidaf = BidafAttn(hidden_size_1 * 3, method="dot")  # no parameter for dot
         self.lstm_raw = RNNEncoder(300, hidden_size_1, bidirectional=True, dropout_p=0, n_layers=1, rnn_type="lstm")
 
-        print("activate video stream")
         self.video_fc = nn.Sequential(
             nn.Dropout(0.5),
             nn.Linear(vid_feat_size, embedding_size),
             nn.Tanh(),
         )
         self.lstm_mature_vid = RNNEncoder(
-            hidden_size_1 * 2 * 5, hidden_size_2, bidirectional=True, 
+            hidden_size_1 * 2 * 3, hidden_size_2, bidirectional=True, 
             dropout_p=0, n_layers=1, rnn_type="lstm")
-        self.classifier_vid = MLP(hidden_size_2*2, 1, 500, n_layers_cls)
+        self.classifier_vid = MLP(hidden_size_2*2, num_classes, 500, n_layers_cls)
         self.config = config
 
     def load_embedding(self, pretrained_embedding):
         self.embedding.weight.data.copy_(torch.from_numpy(pretrained_embedding))
 
-    def forward(self, q, q_l, a0, a0_l, a1, a1_l, a2, a2_l, a3, a3_l, a4, a4_l,
-                vid, vid_l):
+    def forward(self, q, q_l, vid, vid_l):
         e_q = self.embedding(q)
-        e_a0 = self.embedding(a0)
-        e_a1 = self.embedding(a1)
-        e_a2 = self.embedding(a2)
-        e_a3 = self.embedding(a3)
-        e_a4 = self.embedding(a4)
-
         raw_out_q, _ = self.lstm_raw(e_q, q_l)
-        raw_out_a0, _ = self.lstm_raw(e_a0, a0_l)
-        raw_out_a1, _ = self.lstm_raw(e_a1, a1_l)
-        raw_out_a2, _ = self.lstm_raw(e_a2, a2_l)
-        raw_out_a3, _ = self.lstm_raw(e_a3, a3_l)
-        raw_out_a4, _ = self.lstm_raw(e_a4, a4_l)
-
         e_vid = self.video_fc(vid)
         raw_out_vid, _ = self.lstm_raw(e_vid, vid_l)
         vid_out = self.stream_processor(self.lstm_mature_vid, self.classifier_vid, raw_out_vid, vid_l,
-                                        raw_out_q, q_l, raw_out_a0, a0_l, raw_out_a1, a1_l,
-                                        raw_out_a2, a2_l, raw_out_a3, a3_l, raw_out_a4, a4_l)
-
+                                        raw_out_q, q_l)
         out = vid_out
         return out.squeeze()
 
-    def stream_processor(self, lstm_mature, classifier, ctx_embed, ctx_l,
-                         q_embed, q_l, a0_embed, a0_l, a1_embed, a1_l, a2_embed, a2_l, a3_embed, a3_l, a4_embed, a4_l):
+    def stream_processor(self, lstm_mature, classifier, ctx_embed, ctx_l, q_embed, q_l):
         u_q, _ = self.bidaf(ctx_embed, ctx_l, q_embed, q_l)
-        u_a0, _ = self.bidaf(ctx_embed, ctx_l, a0_embed, a0_l)
-        u_a1, _ = self.bidaf(ctx_embed, ctx_l, a1_embed, a1_l)
-        u_a2, _ = self.bidaf(ctx_embed, ctx_l, a2_embed, a2_l)
-        u_a3, _ = self.bidaf(ctx_embed, ctx_l, a3_embed, a3_l)
-        u_a4, _ = self.bidaf(ctx_embed, ctx_l, a4_embed, a4_l)
-
-        concat_a0 = torch.cat([ctx_embed, u_a0, u_q, u_a0 * ctx_embed, u_q * ctx_embed], dim=-1)
-        concat_a1 = torch.cat([ctx_embed, u_a1, u_q, u_a1 * ctx_embed, u_q * ctx_embed], dim=-1)
-        concat_a2 = torch.cat([ctx_embed, u_a2, u_q, u_a2 * ctx_embed, u_q * ctx_embed], dim=-1)
-        concat_a3 = torch.cat([ctx_embed, u_a3, u_q, u_a3 * ctx_embed, u_q * ctx_embed], dim=-1)
-        concat_a4 = torch.cat([ctx_embed, u_a4, u_q, u_a4 * ctx_embed, u_q * ctx_embed], dim=-1)
-
-        mature_maxout_a0, _ = lstm_mature(concat_a0, ctx_l)
-        mature_maxout_a1, _ = lstm_mature(concat_a1, ctx_l)
-        mature_maxout_a2, _ = lstm_mature(concat_a2, ctx_l)
-        mature_maxout_a3, _ = lstm_mature(concat_a3, ctx_l)
-        mature_maxout_a4, _ = lstm_mature(concat_a4, ctx_l)
-
-        mature_maxout_a0 = max_along_time(mature_maxout_a0, ctx_l).unsqueeze(1)
-        mature_maxout_a1 = max_along_time(mature_maxout_a1, ctx_l).unsqueeze(1)
-        mature_maxout_a2 = max_along_time(mature_maxout_a2, ctx_l).unsqueeze(1)
-        mature_maxout_a3 = max_along_time(mature_maxout_a3, ctx_l).unsqueeze(1)
-        mature_maxout_a4 = max_along_time(mature_maxout_a4, ctx_l).unsqueeze(1)
-
-        mature_answers = torch.cat([
-            mature_maxout_a0, mature_maxout_a1, mature_maxout_a2, mature_maxout_a3, mature_maxout_a4
-        ], dim=1)
-        out = classifier(mature_answers)  # (B, 5)
+        concat = torch.cat([ctx_embed, u_q, u_q * ctx_embed], dim=-1)
+        mature_maxout, _ = lstm_mature(concat, ctx_l)
+        mature_maxout = max_along_time(mature_maxout, ctx_l).unsqueeze(1)
+        out = classifier(mature_maxout)  # (B, num_classes)
         return out
 
     @staticmethod
@@ -100,13 +60,9 @@ class ABC(nn.Module):
         bsz = 16
         q = torch.ones(bsz, 25).long().to(device)
         q_l = torch.ones(bsz).fill_(25).long().to(device)
-        a = torch.ones(bsz, 5, 20).long().to(device)
-        a_l = torch.ones(bsz, 5).fill_(20).long().to(device)
-        a0, a1, a2, a3, a4 = [a[:, i, :] for i in range(5)]
-        a0_l, a1_l, a2_l, a3_l, a4_l = [a_l[:, i] for i in range(5)]
         vid = torch.ones(bsz, 100, 2048).to(device)
         vid_l = torch.ones(bsz).fill_(100).long().to(device)
-        return q, q_l, a0, a0_l, a1, a1_l, a2, a2_l, a3, a3_l, a4, a4_l, vid, vid_l
+        return q, q_l, vid, vid_l
 
 
 if __name__ == '__main__':
@@ -117,6 +73,7 @@ if __name__ == '__main__':
         "vid_feat_size": 2048,
         "embed_size": 300,
         "input_size": 100,
+        "output_size": 21,
         "device": "cuda:0",
     }
     model = ABC(config)
