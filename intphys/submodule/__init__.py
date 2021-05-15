b59aad892e1f549e02f93542ce834ea9b184a842
Ilker Kesen <ilkerksn@gmail.com>
2021-05-15 13:37:09 +0300

    implement resnet101, change dropout prob

--- intphys/submodule/__init__.py
+++ intphys/submodule/__init__.py
@@ -1,5 +1,5 @@
 from .base import MLP
-from .visual import CNN2Dv1, resnet18
+from .visual import CNN2Dv1, resnet18, resnet101
 from .textual import LSTMEncoder
 from .resnet3d import r3d_18
 from .misc import FiLMLayer, FiLMBlock2D
@@ -9,6 +9,7 @@ __all__ = (
     "MLP",
     "CNN2Dv1",
     "resnet18",
+    "resnet101",
     "LSTMEncoder",
     "r3d_18",
     "FiLMLayer",
